c46854592c116b13b353e5e4278f3729a413c510
gaucho95 <aschmitt@yahoo.com>
2016-03-29 11:51:13 -0400

    Remove display of excerpt for each post.
    Twentysixteen uses The Excerpt as a byline for each post. We want to reserve use of the excerpt as an alternative to display when content is protected.

--- content.php
+++ content.php
@@ -17,7 +17,7 @@
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 	</header><!-- .entry-header -->
 
-	<?php twentysixteen_excerpt(); ?>
+	<?php /* twentysixteen_excerpt(); */ ?>
 
 	<?php twentysixteen_post_thumbnail(); ?>
 
